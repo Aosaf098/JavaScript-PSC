3bf62098c24febf56d7b0ac991112c872d815c97
Aosaf098 <aosafibad10@gmail.com>
2024-09-02 20:20:43 +0600

    Second Commit

---
+++ Basic/filename-extension.js
@@ -0,0 +1,10 @@
+// Write a JavaScript exercise to get the filename extension.
+
+
+const file = "react.js"
+const file2 = "laravel.php"
+
+console.log(file.split('.').pop())
+console.log(file2.split('.').pop())
+
+
